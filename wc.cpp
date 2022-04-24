40cff0a82a6bd68fcda54fe5c57f2dc294d0c8a0
andi-cho <andrea.cho456@gmail.com>
2022-04-24 17:23:50 -0400

    init commit

---
+++ wc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// function declarations
+
+int main() {
+	char file;
+	cout << "Enter a file name: "; 
+	cin >> file;
+	cout << "The file contains " << num_words << " words." << endl;
+	return 0;
+}
