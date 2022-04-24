cd384d979862524d803f990e82f8b0ce000950b6
andi-cho <andrea.cho456@gmail.com>
2022-04-24 17:26:25 -0400

    dummy func

--- wc.cpp
+++ wc.cpp
@@ -3,11 +3,21 @@
 using namespace std;
 
 // function declarations
+int count_words_in_file(char); 
 
 int main() {
 	char file;
+	int num_words;
+
 	cout << "Enter a file name: "; 
 	cin >> file;
+	num_words = count_words_in_file(file);
+
 	cout << "The file contains " << num_words << " words." << endl;
 	return 0;
+}
+
+int count_words_in_file(char file) 
+{
+	return 123;
 }
